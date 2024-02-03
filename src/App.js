7270e93aaa7701a679931560cc001bd78ba84e3f
Marcelo Hadad <marcelo.hadad@outlook.com>
2024-02-03 20:02:39 -0300

    feat: Create the Side Menu component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { useState } from "react";
+
 import "./styles/App.css";
 import "./styles/reset.css";
 
 import { makeRequest } from "./api/api";
-import { useState } from "react";
+import { SideMenu } from "./components/SideMenu/SideMenu";
 
 function App() {
   return (
     <div className="App">
+      <SideMenu />
       <h1>It's alive!</h1>
     </div>
   );
